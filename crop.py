a14560e96d383756131295bf8af8a1d083ee33df
Shamir Asharf <shamirkolakkadan26@gmail.com>
2025-01-24 16:45:30 +0530

    Update crop.py

--- crop.py
+++ crop.py
@@ -4,17 +4,14 @@ def crop_to_square():
     input_path = "./image/input.png"
     output_path = "./image/cropped.png"
 
-    # Load the input image
     image = cv2.imread(input_path)
     
     if image is None:
         print("Error: Input image not found!")
         return
 
-    # Get the dimensions of the image
     height, width = image.shape[:2]
 
-    # Determine the size of the square
     if width > height:
         diff = (width - height) // 2
         cropped_image = image[:, diff:diff + height]
